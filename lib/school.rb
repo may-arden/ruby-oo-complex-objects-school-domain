14b85cef85b7efb38f965c7119ad7b7ef13c7611
may arden <mayard3n@gmail.com>
2020-11-03 16:51:37 -0800

    commit tree 21c3c1327665464d294ff05f14d6d003df3c9d22

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+require 'pry'
+
+# factory of students categorized by grade & name 
+class School
+
+    attr_accessor :name, :roster 
+    
+ 
+    def initialize(name)
+        @name = name 
+        @roster = {}
+        # binding.pry 
+    end 
+
+
+    def add_student(student_name, grade)
+        roster[grade] ||= []
+        roster[grade].push(student_name)
+
+    end 
+
+    def grade(grade)
+        roster[grade]
+    end 
+
+    def sort
+        sorted_by_grade_hash = {}
+        roster.each do |grade, student_array|
+            sorted_by_grade_hash[grade] = student_array.sort 
+        end 
+        sorted_by_grade_hash
+    end 
+
+end
+
